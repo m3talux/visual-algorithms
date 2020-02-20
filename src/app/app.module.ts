46c375360f435288ce7b6d5af640e0b4b6bd088e
Alex Khoury <alex@polarstork.com>
2020-02-20 23:44:07 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuickSortComponent } from './quick-sort/quick-sort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuickSortComponent
   ],
   imports: [
     BrowserModule,
